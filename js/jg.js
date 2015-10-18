96596f84ee5d1316b20fccce22670e21ff911c53
Qboss <jakgadek@gmail.com>
2015-10-18 21:38:41 +0200

    zad2: naniesienie bloczków na element canvas

---
+++ js/jg.js
@@ -0,0 +1,47 @@
+function draw() {
+    var canvas = document.getElementById('canvas');
+    var context = canvas.getContext('2d');
+
+
+    var y = 60;
+    rezerwacja(1, context, 150, y, 100, 'Jakub Gądek');
+    rezerwacja(1, context, 350, y, 200, 'Jakub Gądek 2');
+    rezerwacja(1, context, 650, y, 150, 'Jakub Gądek 3');
+    rezerwacja(1, context, 900, y, 150, 'Jakub Gądek 4');
+    rezerwacja(1, context, 1300, y, 150, 'Jakub Gądek 5');
+
+    rezerwacja(2, context, 150, y, 200, 'Jakub Gądek');
+    rezerwacja(2, context, 450, y, 150, 'Jakub Gądek 2');
+    rezerwacja(2, context, 700, y, 100, 'Jakub Gądek 3');
+
+    rezerwacja(3, context, 150, y, 150, 'Jakub Gądek');
+    rezerwacja(3, context, 400, y, 100, 'Jakub Gądek 2');
+    rezerwacja(3, context, 600, y, 200, 'Jakub Gądek 3');
+}
+/**
+ * 
+ * @param {type} lvl - poziom na którym ma się rysować element
+ * @param {type} context - element canvas
+ * @param {type} x - początkowy punkt prostokąta na osi x
+ * @param {type} y - początkowy punkt prostokąta na osi y
+ * @param {type} l - długość prostokąta
+ * @param {type} text - tekst
+ * @returns {undefined}
+ */
+function rezerwacja(lvl, context, x, y, l, text) {
+    context.beginPath();
+    context.strokeStyle = 'green';
+    context.lineWidth = 1;
+    context.fillStyle = 'green';
+    context.moveTo(x, lvl * y);
+    context.lineTo(x - 30, lvl * y + 20);
+    context.lineTo(x, lvl * y + 40);
+    context.moveTo(x + l, lvl * y);
+    context.lineTo(x + l + 30, lvl * y + 20);
+    context.lineTo(x + l, lvl * y + 40);
+    context.fillRect(x, lvl * y, l, 40);
+    context.fillStyle = 'white';
+    context.fillText(text, x + 10, lvl * y + 20);
+    context.stroke();
+}
+
